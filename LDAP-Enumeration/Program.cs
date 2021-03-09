31f123b2b1f952f2fcaf64533bb527a04bdcd804
josh mcdaniel <joshrmcdaniel@protonmail.com>
2021-03-09 12:46:43 -0600

    first commit

---
+++ LDAP-Enumeration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LDAP_Enumeration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
